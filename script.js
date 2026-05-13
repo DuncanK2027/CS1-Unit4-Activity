e244a13f282855d0e12fcd1244f0c61b7e09794d
DuncanK2027 <duncan.karcher2027@gbwl.org>
2026-05-13 15:36:33 +0000

    Hacker Mode + Rainbow Mode

--- script.js
+++ script.js
@@ -52,3 +52,26 @@ function toggleMode() {
 
 // Attach function to the button
 toggleBtn.addEventListener("click", toggleMode);
+
+// REVIEW
+// 1. Select an HTML element for JS to target
+const hackerBtn = document.querySelector("#hacker");
+// 2. Attach click event listener to trigger a function
+hackerBtn.addEventListener("click", setHackerMode);
+// 3. Define what happens when event triggers function
+function setHackerMode() {
+    body.style.background = "linear-gradient(black, green)";
+    body.style.fontFamily = "monospace";
+    description.style.color = "rgb(85, 255, 0)";
+    description.textContent = "YOU HAVE BEEN HACKED :(";
+}
+
+
+const rainbowBtn = document.querySelector("#rainbow");
+rainbowBtn.addEventListener("click", setRainbowMode);
+function setRainbowMode() {
+    body.style.background = "linear-gradient(red, orange, yellow, green, blue, purple)";
+    body.style.fontFamily = "copperplate";
+    description.style.color = "rgb(0, 0, 0)";
+    description.textContent = "RAINBOW BLAST!";
+}
